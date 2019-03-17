6afb945c39102e955bc20172925d199b1cf872d3
szajna.marcin12@gmial.com <Mariush12>
2019-03-17 18:28:39 +0100

    TODO list: - -
    FRONTEND:
    
    New features:
    Dark theme
    
    BACKEND:

--- app/src/main/java/com/inc/dayplanner/MainActivity.java
+++ app/src/main/java/com/inc/dayplanner/MainActivity.java
@@ -8,14 +8,19 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Switch;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
-
+    private Switch sw;
+    private Menu menu;
+    private MenuItem itemSwitch;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +28,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Menu");
+        //  setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -36,33 +42,67 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        System.out.println("lol");
-        System.out.println("twoja st ara asfasdffdsafdsafsdafdssafsd");
+
+        menu = navigationView.getMenu();
+        itemSwitch = menu.findItem(R.id.app_bar_switch);
+        itemSwitch.setActionView(R.layout.switch_item);
+        sw = menu.findItem(R.id.app_bar_switch).getActionView().findViewById(R.id.switcher);
+
 
     }
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
         switch (menuItem.getItemId()) {
             case R.id.nav_message:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new opt1Fragment(),null).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new opt1Fragment(), null).commit();
+                drawer.closeDrawer(GravityCompat.START);
+                toolbar.setTitle("Message");
                 break;
+
+            case R.id.app_bar_switch:
+                switchButtonHandler(sw);
+                break;
+
         }
-        drawer.closeDrawer(GravityCompat.START);
+
 
         return true;
     }
 
     @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }else {
+        } else {
             super.onBackPressed();
         }
 
     }
+
+    public void switchButtonHandler(Switch sw) {
+
+        View menuItems = findViewById(R.id.nav_view);
+        View toolobar = findViewById(R.id.toolbar);
+
+        if (sw.isChecked()) {
+            sw.setChecked(false);
+            drawer.setBackgroundColor(getResources().getColor(R.color.CreamBackground));
+            menuItems.setBackgroundColor(getResources().getColor(R.color.CreamBackground));
+            toolobar.setBackgroundColor(getResources().getColor(R.color.CreamBackground));
+
+
+        } else {
+            sw.setChecked(true);
+            drawer.setBackgroundColor(getResources().getColor(R.color.DarkToolbar));
+            menuItems.setBackgroundColor(getResources().getColor(R.color.DarkToolbar));
+            toolobar.setBackgroundColor(getResources().getColor(R.color.DarkBackground));
+        }
+
+
+    }//#564559
 
 }
 
