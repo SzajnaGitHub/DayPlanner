a72bfcecf18d59f38054c8db9345121478fcad58
ksewer <kacper.seweryn18@gmail.com>
2019-04-08 22:51:53 +0200

    added classes to SignIn Google Account

--- app/src/main/java/com/inc/dayplanner/GoogleDriveFileHolder.java
+++ app/src/main/java/com/inc/dayplanner/GoogleDriveFileHolder.java
@@ -1,62 +1,329 @@
 package com.inc.dayplanner;
 
-import com.google.api.client.util.DateTime;
+import com.google.android.gms.drive.CreateFileActivityOptions;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.OpenFileActivityBuilder;
+import com.google.android.gms.drive.OpenFileActivityOptions;
+import com.inc.dayplanner.tasks.EditDriveFileAsyncTask;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 
-public class GoogleDriveFileHolder {
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-    private String id;
-    private String name;
-    private DateTime modifiedTime;
-    private long size;
-    private DateTime createdTime;
-    private Boolean starred;
+import java.io.IOException;
+import java.util.Collections;
 
+public class GoogleDriveFileHolder extends BaseDriveActivity {
+    private static final String TAG = "MainActivity";
 
-    public DateTime getCreatedTime() {
-        return createdTime;
+    /**
+     * Request code for file creator activity.
+     */
+    private static final int REQUEST_CODE_CREATOR = NEXT_AVAILABLE_REQUEST_CODE;
+
+    /**
+     * Request code for the file opener activity.
+     */
+    private static final int REQUEST_CODE_OPENER = NEXT_AVAILABLE_REQUEST_CODE + 1;
+
+    /**
+     * Text file MIME type.
+     */
+    private static final String MIME_TYPE_TEXT = "text/plain";
+
+    /**
+     * Drive ID of the currently opened Drive file.
+     */
+    private DriveId mCurrentDriveId;
+
+    /**
+     * Currently opened file's metadata.
+     */
+    private Metadata mMetadata;
+
+    /**
+     * Currently opened file's contents.
+     */
+    private DriveContents mDriveContents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.google_drive_file_holder);
+
+//        mTitleEditText = (EditText) findViewById(R.id.editTextTitle);
+//        mContentsEditText = (EditText) findViewById(R.id.editTextContents);
+//        mSaveButton = (Button) findViewById(R.id.buttonSave);
+//        mSaveButton.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                save();
+//            }
+//        });
+        refreshUiFromCurrentFile();
     }
 
-    public void setCreatedTime(DateTime createdTime) {
-        this.createdTime = createdTime;
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.activity_main, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == R.id.menu_new) {
+//            Log.i(TAG, "Create file selected.");
+//            createDriveFile();
+//        } else if (item.getItemId() == R.id.menu_open) {
+//            Log.i(TAG, "Open file selected.");
+//            openDriveFile();
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_CREATOR:
+            case REQUEST_CODE_OPENER:
+                if (resultCode == RESULT_OK) {
+                    mCurrentDriveId = (DriveId) data.getParcelableExtra(
+                            OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
+                    loadCurrentFile();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
-    public Boolean getStarred() {
-        return starred;
+    /**
+     * Refreshes the main content view with the current activity state.
+     */
+    private void refreshUiFromCurrentFile() {
+        Log.d(TAG, "Refreshing...");
+        if (mCurrentDriveId == null) {
+ //           mSaveButton.setEnabled(false);
+            return;
+        }
+ //       mSaveButton.setEnabled(true);
+
+        if (mMetadata == null || mDriveContents == null) {
+            return;
+        }
+
+  //      mTitleEditText.setText(mMetadata.getTitle());
+        try {
+            String contents = ReadFile.readFromInputStream(mDriveContents.getInputStream());
+  //          mContentsEditText.setText(contents);
+        } catch (IOException e) {
+            Log.e(TAG, "IOException while reading from contents input stream", e);
+            showToast(R.string.msg_errreading);
+  //          mSaveButton.setEnabled(false);
+        }
     }
 
-    public void setStarred(Boolean starred) {
-        this.starred = starred;
+    /**
+     * Retrieves the currently selected Drive file's metadata and contents.
+     */
+    private void loadCurrentFile() {
+        Log.d(TAG, "Retrieving...");
+        final DriveFile file = mCurrentDriveId.asDriveFile();
+
+        // Retrieve and store the file metadata and contents.
+        mDriveResourceClient.getMetadata(file)
+                .continueWithTask(new Continuation<Metadata, Task<DriveContents>>() {
+                    @Override
+                    public Task<DriveContents> then(@NonNull Task<Metadata> task) {
+                        if (task.isSuccessful()) {
+                            mMetadata = task.getResult();
+                            return mDriveResourceClient.openFile(file, DriveFile.MODE_READ_ONLY);
+                        } else {
+                            return Tasks.forException(task.getException());
+                        }
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<DriveContents>() {
+            @Override
+            public void onSuccess(DriveContents driveContents) {
+                mDriveContents = driveContents;
+                refreshUiFromCurrentFile();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "Unable to retrieve file metadata and contents.", e);
+            }
+        });
     }
 
-    public String getId() {
-        return id;
+    String content="tresc pliku";
+    String title="tytul";
+    /**
+     * Saves metadata and content changes.
+     */
+    public void save() {
+        Log.d(TAG, "Saving...");
+
+        if (mCurrentDriveId == null) {
+            return;
+        }
+
+        new EditDriveFileAsyncTask(mDriveResourceClient) {
+            @Override
+            public Changes edit(DriveContents driveContents) {
+                MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
+                        .setTitle(title)
+                        .build();
+                try {
+                    byte[] body = content.getBytes();
+                    driveContents.getOutputStream().write(body);
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException while reading from driveContents output stream", e);
+                }
+                return new Changes(metadataChangeSet, driveContents);
+            }
+
+            @Override
+            protected void onPostExecute(Boolean isSuccess) {
+                if (isSuccess) {
+                    showToast(R.string.msg_saved);
+                } else {
+                    showToast(R.string.msg_errsaving);
+                }
+            }
+        }.execute(mCurrentDriveId);
     }
 
-    public void setId(String id) {
-        this.id = id;
+    /**
+     * Shows a {@link Toast} with the given message.
+     */
+    private void showToast(int id) {
+        Toast.makeText(this, id, Toast.LENGTH_LONG).show();
     }
 
-    public String getName() {
-        return name;
+    /**
+     * Launches an {@link Intent} to create a new Drive file.
+     */
+    private void createDriveFile() {
+        Log.i(TAG, "Create drive file.");
+
+        if (!isSignedIn()) {
+            Log.w(TAG, "Failed to create file, user is not signed in.");
+            return;
+        }
+
+        // Nullify the previous DriveContents and Metadata
+        mDriveContents = null;
+        mMetadata = null;
+
+        // Build the DriveContents and start a CreateFileActivityIntent.
+        mDriveResourceClient.createContents()
+                .continueWithTask(new Continuation<DriveContents, Task<IntentSender>>() {
+                    @Override
+                    public Task<IntentSender> then(@NonNull Task<DriveContents> task) {
+                        if (!task.isSuccessful()) {
+                            return Tasks.forException(task.getException());
+                        }
+                        Log.i(TAG, "New contents created.");
+                        // Build file metadata options.
+                        MetadataChangeSet metadataChangeSet =
+                                new MetadataChangeSet.Builder()
+                                        .setMimeType(MIME_TYPE_TEXT)
+                                        .build();
+                        // Build file creation options.
+                        CreateFileActivityOptions createFileActivityOptions =
+                                new CreateFileActivityOptions.Builder()
+                                        .setInitialMetadata(metadataChangeSet)
+                                        .setInitialDriveContents(task.getResult())
+                                        .build();
+                        // Build CreateFileActivityIntent.
+                        return mDriveClient.newCreateFileActivityIntentSender(createFileActivityOptions);
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<IntentSender>() {
+            @Override
+            public void onSuccess(IntentSender intentSender) {
+                Log.i(TAG, "New CreateActivityIntent created.");
+                try {
+                    // Start CreateFileActivityIntent
+                    startIntentSenderForResult(
+                            intentSender,
+                            REQUEST_CODE_CREATOR,
+                            /* fillInIntent= */ null,
+                            /* flagsMask= */ 0,
+                            /* flagsValues= */ 0,
+                            /* extraFlags= */ 0);
+                } catch (SendIntentException e) {
+                    Log.e(TAG, "Failed to launch file chooser.", e);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "Failed to create file.", e);
+            }
+        });
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    /**
+     * Launches an {@link Intent} to open an existing Drive file.
+     */
+    private void openDriveFile() {
+        Log.i(TAG, "Open Drive file.");
 
-    public DateTime getModifiedTime() {
-        return modifiedTime;
-    }
+        if (!isSignedIn()) {
+            Log.w(TAG, "Failed to open file, user is not signed in.");
+            return;
+        }
 
-    public void setModifiedTime(DateTime modifiedTime) {
-        this.modifiedTime = modifiedTime;
-    }
+        // Build activity options.
+        final OpenFileActivityOptions openFileActivityOptions =
+                new OpenFileActivityOptions.Builder()
+                        .setMimeType(Collections.singletonList(MIME_TYPE_TEXT))
+                        .build();
 
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
+        // Start a OpenFileActivityIntent
+        mDriveClient.newOpenFileActivityIntentSender(openFileActivityOptions)
+                .addOnSuccessListener(new OnSuccessListener<IntentSender>() {
+                    @Override
+                    public void onSuccess(IntentSender intentSender) {
+                        try {
+                            startIntentSenderForResult(
+                                    intentSender,
+                                    REQUEST_CODE_OPENER,
+                                    /* fillInIntent= */ null,
+                                    /* flagsMask= */ 0,
+                                    /* flagsValues= */ 0,
+                                    /* extraFlags= */ 0);
+                        } catch (SendIntentException e) {
+                            Log.w(TAG, "Unable to send intent.", e);
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "Unable to create OpenFileActivityIntent.", e);
+            }
+        });
     }
 }
--- app/src/main/java/com/inc/dayplanner/LoginActivity.java
+++ app/src/main/java/com/inc/dayplanner/LoginActivity.java
@@ -1,35 +1,17 @@
 package com.inc.dayplanner;
 
 import android.content.Intent;
-import android.content.IntentSender;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.drive.Drive;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.drive.DriveScopes;
-
-import java.util.Collections;
-
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
 
-    private DriveServiceHelper mDriveServiceHelper;
+public class LoginActivity extends AppCompatActivity {
+
+
+
     private static final String TAG = "Google Drive Activity";
-    private GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,84 +20,13 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
 
     public void Login(View view) {
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        logInGoogleAcc();
+        BaseDriveActivity baseDriveActivity = new BaseDriveActivity();
+        baseDriveActivity.signIn();
+        //Intent intent = new Intent(this, GoogleDriveFileHolder.class);
+        //startActivity(intent);
     }
 
-    public void logInGoogleAcc() {
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(LoginActivity.this);
 
-        GoogleAccountCredential credential =
-                GoogleAccountCredential.usingOAuth2(LoginActivity.this, Collections.singleton(DriveScopes.DRIVE_FILE));
-        credential.setSelectedAccount(account.getAccount());
-        com.google.api.services.drive.Drive googleDriveService =
-                new com.google.api.services.drive.Drive.Builder(
-                        AndroidHttp.newCompatibleTransport(),
-                        new GsonFactory(),
-                        credential)
-                        .setApplicationName("AppName")
-                        .build();
-        mDriveServiceHelper = new DriveServiceHelper(googleDriveService);
-
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(Drive.API)
-                    .addScope(Drive.SCOPE_FILE)
-                    .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
-                    .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
-                    .build();
-        }
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.disconnect();
-        }
-        super.onPause();
-    }
-    public void onClickLogout(View view) {
-        mGoogleApiClient.clearDefaultAccountAndReconnect().setResultCallback((new ResultCallback<Status>() {
-            @Override
-            public void onResult(@NonNull Status status) {
-                Toast.makeText(getApplicationContext(), "Wylogowano", Toast.LENGTH_LONG).show();
-            }
-        }));
-    }
-
-    private static final int REQUEST_CODE_RESOLUTION = 1;
-
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        Toast.makeText(getApplicationContext(), "Zalogowano ", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        Log.i(TAG, "GoogleApiClient połączenie nieudane: " + result.toString());
-        if (result.hasResolution()) {
-            try {
-                result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
-            } catch (IntentSender.SendIntentException e) {
-                e.printStackTrace();
-            }
-        } else {
-            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        Log.i(TAG, "GoogleApiClient połączenie wstrzymane.");
-    }
 
 }
 
